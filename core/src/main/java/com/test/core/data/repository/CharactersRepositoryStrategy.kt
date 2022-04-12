c5c829b23e7556a8757a20a505a93892753aea0c
Alex Bieliaiev <alex.bieliaiev@digidentity.com>
2022-04-12 17:41:20 +0200

    minor re-organization

--- core/src/main/java/com/test/core/data/repository/CharactersRepositoryStrategy.kt
+++ core/src/main/java/com/test/core/data/repository/CharactersRepositoryStrategy.kt
@@ -11,3 +11,58 @@ internal interface CharactersRepositoryStrategy {
     fun getCharacters(offset: Int): BreakingBadCharacterListResult
 
 }
+
+internal abstract class CharactersRepositoryStrategyWithAnUpstream : CharactersRepositoryStrategy {
+
+    protected abstract val upstream: CharactersRepositoryStrategy
+
+    override fun clearCharacters() = clearCharactersImpl().also {
+        upstream.clearCharacters()
+    }
+
+    override fun getCharacter(id: Int): BreakingBadCharacter? {
+        var character = loadCharacter(id)
+        if (character == null) {
+            character = fetchCharacterFromTheUpstream(id)
+        }
+
+        return character
+    }
+
+    override fun getCharacters(): BreakingBadCharacterListResult {
+        val characters = loadCharacters()
+        return if (characters.isEmpty()) {
+            fetchCharactersFromTheUpstream()
+        } else {
+            BreakingBadCharacterListResult(
+                characters = characters,
+                hasMore = true
+            )
+        }
+    }
+
+    override fun getCharacters(offset: Int): BreakingBadCharacterListResult {
+        return upstream.getCharacters(offset).also {
+            setCharacters(it.characters)
+        }
+    }
+
+    protected abstract fun clearCharactersImpl()
+
+    protected abstract fun loadCharacter(id: Int): BreakingBadCharacter?
+
+    protected abstract fun loadCharacters(): List<BreakingBadCharacter>
+
+    protected abstract fun setCharacter(character: BreakingBadCharacter)
+
+    protected abstract fun setCharacters(characters: List<BreakingBadCharacter>)
+
+    private fun fetchCharacterFromTheUpstream(id: Int) = upstream.getCharacter(id)?.also {
+        setCharacter(it)
+    }
+
+    private fun fetchCharactersFromTheUpstream() = upstream.getCharacters().also {
+        setCharacters(it.characters)
+    }
+
+}
